fd0f7be7ea713c90a541e72bb508609756958523
Julio Cesar Pedroso <juliocp@weg.net>
2022-03-21 12:56:27 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.devsuperior.dscatalog.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +18,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
-    
+
     public final CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll(){
+    public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).toList();
     }
@@ -29,20 +31,35 @@ public class CategoryService {
     public CategoryDTO findById(Long id) {
 
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 
         return new CategoryDTO(entity);
     }
 
     @Transactional
     public CategoryDTO insert(CategoryDTO dto) {
-        
+
         Category entity = new Category();
         entity.setName(dto.getName());
         entity = repository.save(entity);
-        
+
         return new CategoryDTO(entity);
 
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+
+        try {
+            Category entity = repository.getById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+
+        }
     }
 
 }
