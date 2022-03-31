6cc176b2150cec68673e4e8c5077fb36899f0980
SnoopyBR <juliopedroso@gmail.com>
2022-03-30 22:44:26 -0300

    Exercise executed

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -5,6 +5,11 @@ import static org.mockito.Mockito.*;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
@@ -20,8 +25,10 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockedStatic.Verification;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -44,6 +51,8 @@ class ProductServiceTests {
     private Long dependentId;
     private PageImpl<Product> page;
     private Product product;
+    private ProductDTO productDTO;
+    private CategoryDTO categoryDTO;
 
     @BeforeEach
     void setup() throws Exception{
@@ -51,6 +60,8 @@ class ProductServiceTests {
         nonExistingId = 1000L;
         dependentId = 4L;
         product = Factory.createProduct();
+        productDTO = Factory.createProductDTO();
+        categoryDTO = Factory.createCategoryDTO();
         page = new PageImpl<>(List.of(product));
 
         Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
@@ -59,6 +70,11 @@ class ProductServiceTests {
 
         Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
         Mockito.when(repository.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
+        Mockito.when(repository.getById(existingId)).thenReturn(product);
+        Mockito.when(repository.getById(nonExistingId)).thenThrow(EntityNotFoundException.class);
+
+        Mockito.when(categoryRepository.getById(existingId)).thenReturn(new Category(2L, "Electronics"));
 
         doNothing().when(repository).deleteById(existingId);
         doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
@@ -91,6 +107,58 @@ class ProductServiceTests {
         });
 
         verify(repository, Mockito.times(1)).deleteById(dependentId);
+    }
+
+    @Test
+    void findAllPagedShouldReturnPage(){
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ProductDTO> result = service.findAllPaged(pageable);
+
+        Assertions.assertNotNull(result);
+
+        verify(repository).findAll(pageable);
+    }
+
+    @Test
+    void findByIdShouldReturnProductDTOWhenIdExists(){
+        ProductDTO productDTO = service.findById(existingId);
+
+        Assertions.assertNotNull(productDTO);
+
+        verify(repository,Mockito.times(1)).findById(existingId);
+    }
+
+    @Test
+    void findByIdShouldThrowResouceNotFoundExceptionWhenIdDoesNotExist(){
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.findById(nonExistingId);
+        });
+
+        verify(repository,Mockito.times(1)).findById(nonExistingId);
+        
+    }
+
+    @Test
+    void updateShouldReturnProductDTOWhenIdExists(){
+
+        ProductDTO entity =  service.update(existingId, productDTO);
+
+        Assertions.assertNotNull(entity);
+
+        verify(repository,Mockito.times(1)).getById(existingId);
+                
+    }
+
+    @Test
+    void updateShouldThrowResouceNotFoundExceptionWhenIdDoesNotExist(){
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.update(nonExistingId, productDTO);
+        });
+
+        verify(repository,Mockito.times(1)).getById(nonExistingId);
+        
     }
 
 }
